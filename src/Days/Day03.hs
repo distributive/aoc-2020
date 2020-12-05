7ded5ae129d40789189032bc1a041693f791a7ef
Amelie <github@ameliewd.com>
2020-12-05 00:35:24 +0000

    Day 3 solutions.

--- src/Days/Day03.hs
+++ src/Days/Day03.hs
@@ -3,6 +3,7 @@ module Days.Day03 (runDay) where
 {- ORMOLU_DISABLE -}
 import Data.List
 import Data.Map.Strict (Map)
+import Data.Map.Strict (findWithDefault)
 import qualified Data.Map.Strict as Map
 import Data.Maybe
 import Data.Set (Set)
@@ -14,6 +15,8 @@ import qualified Util.Util as U
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
 import Data.Void
+
+import Util.Parsers (coordinateParser)
 {- ORMOLU_ENABLE -}
 
 runDay :: Bool -> String -> IO ()
@@ -21,19 +24,25 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = coordinateParser mapTree 0
+    where
+        mapTree '.' = Just 0
+        mapTree '#' = Just 1
+        mapTree  _  = Nothing
 
 ------------ TYPES ------------
-type Input = Void
+type Input = (Map (Int, Int) Int)
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = [Int]
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA input = length . filter (1==) $ [ findWithDefault 0 (y*3 `mod` 31, y) input | y <- [0..322] ]
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB input = map findForRatio [(1,1), (3,1), (5,1), (7,1), (1,2)]
+    where
+        findForRatio (a,b) = length . filter (1==) $ [ findWithDefault 0 (x `mod` 31, y) input | y <- [0..322], x <- [0..(322*a `div` b)], b*x == a*y ]
