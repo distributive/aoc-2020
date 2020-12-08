24753678b49cdccb70547e8d18c6909e5a6f60d2
distributive <github@ameliewd.com>
2020-12-08 20:52:58 +0000

    Day 8 solutions.

--- src/Days/Day08.hs
+++ src/Days/Day08.hs
@@ -8,12 +8,17 @@ import Data.Maybe
 import Data.Set (Set)
 import qualified Data.Set as Set
 import Data.Vector (Vector)
+import Data.Vector ((!), (//))
 import qualified Data.Vector as Vec
 import qualified Util.Util as U
 
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
 import Data.Void
+
+import Control.Applicative
+import Control.Monad
+import Control.Monad.State.Lazy
 {- ORMOLU_ENABLE -}
 
 runDay :: Bool -> String -> IO ()
@@ -21,19 +26,63 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = Vec.fromList <$> instruction `sepBy` endOfLine
+    where
+        instruction =
+            choice
+            [
+                string "acc " >> Acc <$> (signed decimal),
+                string "jmp " >> Jmp <$> (signed decimal),
+                string "nop " >> Nop <$> (signed decimal)
+            ]
+
+{- Original, worked out all by myself (the above is modified on suggestion by Sam) -}
+-- inputParser = instruction `sepBy` endOfLine
+--     where
+--         instruction = do
+--             ins <- count 3 letter
+--             skipSpace
+--             value <- signed decimal
+--             return (ins, value)
 
 ------------ TYPES ------------
-type Input = Void
+data Instruction = Acc Int | Jmp Int | Nop Int deriving (Show, Eq)
+data Output = Terminate Int | Loop Int
+type Program = Vector Instruction
 
-type OutputA = Void
+type Input = Program
 
-type OutputB = Void
+type OutputA = Int
+
+type OutputB = Int
+
+---------- INTERPRETER ---------
+runProgram :: Set Int -> Int -> Int -> Program -> Output
+runProgram visited counter accumulator program =
+    if
+        | counter `Set.member` visited -> Loop accumulator
+        | counter >= length program    -> Terminate accumulator
+        | otherwise -> case program!counter of
+            Acc val -> runProgram (Set.insert counter visited) (counter +   1) (accumulator + val) program
+            Jmp val -> runProgram (Set.insert counter visited) (counter + val)  accumulator        program
+            Nop val -> runProgram (Set.insert counter visited) (counter +   1)  accumulator        program
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA input = case runProgram Set.empty 0 0 input of
+    Loop      out -> out
+    Terminate out -> error "Program terminated when a loop was expected"
+
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB input = fromOutput $ head $ filter isTerm $ (runProgram Set.empty 0 0) <$> [swapAt i input | i <- [0..length input]]
+    where
+        fromOutput (Terminate val) = val
+        fromOutput (Loop      val) = val
+        isTerm (Terminate _) = True
+        isTerm (Loop      _) = False
+        swapAt index program = program // [(index, swap $ program!index)]
+        swap (Nop val) = Jmp val
+        swap (Jmp val) = Nop val
+        swap (Acc val) = Acc val
